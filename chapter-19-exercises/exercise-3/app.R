af978f4eb57cdcaec44b900a794015dd83ddfef0
Jaimie Jin <jinjaimie12@gmail.com>
2020-03-09 21:11:01 -0700

    week 8/9 exercises?

--- chapter-19-exercises/exercise-3/app.R
+++ chapter-19-exercises/exercise-3/app.R
@@ -1,9 +1,9 @@
 # Exercise 3: interactive Shiny apps
-
 # Load the shiny, ggplot2, and dplyr libraries
 library("shiny")
 library("ggplot2")
 library("dplyr")
+library("rsconnect")
 
 # You will again be working with the `diamonds` data set provided by ggplot2
 # Use dplyr's `sample_n()` function to get a random 1000 rows from the data set
@@ -12,11 +12,11 @@ diamonds_sample <- sample_n(diamonds, 1000)
 
 # For convenience store the `range()` of values for the `price` column
 # (of your sample)
-
+price_range <- range(diamonds_sample$price)
 
 # For convenience, get a vector of column names from the `diamonds` data set to
 # use as select inputs
-
+col_names <- colnames(diamonds_sample)
 
 # To help keep the code organized, we'll store some UI elements in variables
 # _before_ defining the UI.
@@ -27,50 +27,72 @@ diamonds_sample <- sample_n(diamonds, 1000)
 # - a label of "Price (in dollars)"
 # - min and max valuesvalue based on the `price_range` calculated above
 # - a current value equal to the price range
-
+price_input <- sliderInput(
+  inputId = "price_choice",
+  label = "Price (in dollars)",
+  min = price_range[1],
+  max = price_range[2],
+  value = price_range
+)
 
 # Define a variable `feature_input` that is a `selectInput()` with the
 # label "Feature of Interest". This dropdown should let the user pick one of
 # the columns of the diamond data set. Use the `carat` column as a default
 # Make sure to set an inputId to reference in your server!
-
+feature_input = selectInput(
+  label = "Feature of Interest",
+  inputId = "feature_select",
+  choices = col_names,
+  selected = "carat"
+)
 
 # Define a UI using a `fluidPage()` layout with the following content:
-
+ui <- fluidPage(
+  
   # A `titlePanel` with the title "Diamond Viewer"
-
+  titlePanel(title = "Diamond Viewer"),
   # Your `prince_input`
-
+  price_input,
   # Your `feature_input`
-
+  feature_input,
   # A `checkboxInput()` labeled "Show Trendline". It's default value is TRUE
+  checkboxInput(label = "Show Trendline", inputId = "trendline", value = TRUE),
   
-
   # A plotOutput showing the 'plot' output (based on the user specifications)
-  
-
+  plotOutput(outputId = "plot")
+)
 
 # Define a `server` function (with appropriate arguments)
 # This function should perform the following:
-
+  server <- function(input, output) {
   # Assign a reactive `renderPlot()` function to the outputted 'plot' value
-  
+    output$plot <- renderPlot({
     # This function should first filter down the `diamonds_sample` data
     # using the input price range (remember to get both ends)!
-    
+    selected_range <- input$price_choice
 
     # Use the filtered data set to create a ggplot2 scatter plot with the
     # user-select column on the x-axis, and the price on the y-axis,
     # and encode the "cut" of each diamond using color
     # Save your plot as a variable.
+    data <- diamonds_sample %>% 
+      filter(price > selected_range[1] & price < selected_range[2])
     
+    plot <- ggplot(data = data, mapping = aes_string(x= input$feature_select, y="price", color = "cut")) +
+      geom_point()
+      
     # Finally, if the "trendline" checkbox is selected, you should add (+)
     # a geom_smooth geometry (with `se=FALSE`) to your plot
     # Hint: use an if statement to see if you need to add more geoms to the plot
-
-  
+    if (input$trendline) {
+        plot <- plot +
+          geom_smooth(se = FALSE)
+    }
+    
     # Be sure and return the completed plot!
-  
+      return(plot)
+      }) 
+}
 
 # Create a new `shinyApp()` using the above ui and server
-
+shinyApp(ui = ui, server = server)
